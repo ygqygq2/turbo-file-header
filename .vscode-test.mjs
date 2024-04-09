f89d94bf98afa43853ca1617e95c3fa9cfbb8875
ygqygq2 <ygqygq2@qq.com>
2024-04-09 18:55:21 +0800

    test: 调整 workspace
    Co-authored-by: ygqygq2 <ygqygq2@qq.com>

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,9 @@
 // .vscode-test.mjs
 import { defineConfig } from '@vscode/test-cli';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // suiteTests 使用 vscode-test 测试，当前无法直接支持 ts,它需要编译成js
 // unitTests 直接使用 mocha 测试，直接使用 ts
@@ -8,6 +12,8 @@ export default defineConfig([
     label: 'suiteTests',
     files: 'out/test/suite/*.test.js',
     version: '1.86.0',
+    extensionDevelopmentPath: __dirname,
+    workspaceFolder: `${__dirname}/sampleWorkspace`,
     mocha: {
       ui: 'tdd',
       timeout: 20000,
--- src/test/suite/addFileheader.test.ts
+++ src/test/suite/addFileheader.test.ts
@@ -4,9 +4,17 @@ import path from 'path';
 import * as vscode from 'vscode';
 
 describe('e2e test for command: addFileheader', () => {
+  let workspaceFolder: string;
+
+  before(() => {
+    const projectRoot = path.resolve(__dirname, '../../../');
+    workspaceFolder = path.resolve(projectRoot, './sampleWorkspace');
+    vscode.workspace.updateWorkspaceFolders(0, 0, { uri: vscode.Uri.file(workspaceFolder) });
+  });
+
   it('should add file header to the current file', async () => {
     // 打开一个文件
-    const file = path.join(__dirname, 'files', 'to-test-command-addFileheader.ts');
+    const file = path.join(workspaceFolder, 'files', 'to-test-command-addFileheader.ts');
     const doc = await vscode.workspace.openTextDocument(file);
     // 获取原始文件内容
     const originalContent = doc.getText();
@@ -16,7 +24,6 @@ describe('e2e test for command: addFileheader', () => {
     await vscode.commands.executeCommand('turboFileHeader.addFileheader');
     // 获取新的文件内容
     const newContent = doc.getText();
-    console.log('🚀 ~ file: addFileheader.test.ts:19 ~ newContent:', newContent);
     // 检查文件内容是否已经变化
     assert.notStrictEqual(originalContent, newContent);
   });
